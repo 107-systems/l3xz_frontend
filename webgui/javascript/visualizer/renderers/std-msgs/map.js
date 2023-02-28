11979f4b249dacc11731a83940378711daa83234
wuehr1999 <jonas.wuehr@t-online.de>
2023-02-28 20:02:15 +0100

    export baselib

--- webgui/javascript/visualizer/renderers/std-msgs/map.js
+++
@@ -1,8 +0,0 @@
-/***************************************************************
-This software is distributed under the terms of the MIT License.
-Copyright (c) 2022 107-Systems
-Author: Jonas Wühr (jonaswuehrmaintainer@gmail.com)
-****************************************************************/
-const renderers_std_msgs = [
-        render_std_msgs_Numeric
-    ];
