9957e259452dcb9f9d5a4cab3da99fc637091da8
Jonas Wühr <33281076+wuehr1999@users.noreply.github.com>
2023-02-28 20:56:27 +0100

    Modularization using base submodule (#10)
    * export baselib
    
    * include baselib
    
    * update docs

--- webgui/javascript/visualizer/renderers/nav-msgs/map.js
+++
@@ -1,9 +0,0 @@
-/***************************************************************
-This software is distributed under the terms of the MIT License.
-Copyright (c) 2022 107-Systems
-Author: Jonas Wühr (jonaswuehrmaintainer@gmail.com)
-****************************************************************/
-const renderers_nav_msgs = [
-    render_nav_msgs_Odometry,
-    render_nav_msgs_OccupancyGrid
-];
