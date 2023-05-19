4497160d6dbf5f44d3b6f63efdf90360093d9611
Alexander Entinger <consulting@lxrobotics.com>
2023-05-19 11:33:29 +0200

    Mucho fixo. At least the CI build builds now.

--- setup.py
+++ setup.py
@@ -8,19 +8,20 @@ setup(
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
-         ['resource/' + package_name]),
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
+    zip_safe=True,
     include_package_data=True,
     maintainer='Jonas Wuehr',
     maintainer_email='jonaswuehrmaintainer@gmail.com',
-    description='ROS2 node for openmv camera',
+    description='Web interface for L3X-Z',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'l3xz_openmv_camera = l3xz_openmv_camera.run_webgui:main',
+            'l3xz_frontend = l3xz_frontend.run_webgui:main',
         ],
     },
 )
